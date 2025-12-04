03a80ddbb8afa5e8555a1212c7e94d0b13b125b7
jtorradogal <jtorradogal@gmail.com>
2025-12-04 23:50:27 +0100

    Update shop.js

--- api/shop.js
+++ api/shop.js
@@ -23,6 +23,7 @@ export default async function handler(req, res) {
         email,
         website,
         description,
+        image_url,
         category:category (category),
         place:place_id (placename)
       `)
